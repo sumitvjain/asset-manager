ab633729bde24a81f60389c1046aa3cf852e50b2
sumitvjain <sumitvjain@yahoo.com>
2026-01-30 08:49:27 +0530

    Added handle runtime paths (sys._MEIPASS)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # from qt_lib.qt_compact import *
-import sys
+import sys, os
 from pathlib import Path
 from qt_lib import qt_compact #module 
 from PySide2.QtGui import QFontDatabase
@@ -12,16 +12,43 @@ from config import constant, settings
 
 
 
+def resource_path(relative_path):
+    """
+    Works for:
+    - python main.py
+    - PyInstaller --onefile exe
+    """
+    if hasattr(sys, "_MEIPASS"):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
+
+
+
 
 if __name__ == "__main__":
     app = qt_compact.QApplication(sys.argv)
     app.setStyleSheet(qdarkstyle.load_stylesheet_pyside2())
 
-    qss_file = Path("resources/styles.qss")
-    if qss_file.exists():
-        app.setStyleSheet(app.styleSheet() + qss_file.read_text())
+    # #####################################################################
+    # qss_file = Path("resources/styles.qss")
+    # if qss_file.exists():
+    #     app.setStyleSheet(app.styleSheet() + qss_file.read_text())
+    # else:
+    #     print('no'*10)
+    # #####################################################################
+
+
+    qss_path = resource_path("resources/styles.qss")
+
+    if os.path.exists(qss_path):
+        with open(qss_path, "r", encoding="utf-8") as f:
+            app.setStyleSheet(app.styleSheet() + f.read())
     else:
-        print('no'*10)
+        print("QSS file not found:", qss_path)
+
+
+
 
     view = view.View(app)
     model = model.Model()  
